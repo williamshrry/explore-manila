1f0fdfbb051951006224a3cf72bb3fe8e0bf1dbb
williamshrry <williamsherbert326@gmail.com>
2026-05-11 23:15:15 +0800

    Update photos.js

--- photos.js
+++ photos.js
@@ -2,19 +2,19 @@ const photos = [
 
   {
     image: "images/img1.jpg",
-    orbitAngle: 160,
+    orbitAngle: 45,
     orbitRadius: 60
   },
 
   {
     image: "images/img2.jpg",
-    orbitAngle: 100,
+    orbitAngle: 90,
     orbitRadius: 90
   },
 
   {
     image: "images/img3.jpg",
-    orbitAngle: 330,
+    orbitAngle: 220,
     orbitRadius: 70
   }
 
