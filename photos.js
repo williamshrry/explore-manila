e3292da165ac3a33b4d06a8a350bf99a593b3334
williamshrry <williamsherbert326@gmail.com>
2026-05-11 23:06:27 +0800

    Update photos.js

--- photos.js
+++ photos.js
@@ -1,30 +1,21 @@
 const photos = [
 
   {
-    id: 0,
-    title: "Front Right Perspective",
     image: "images/img1.jpg",
     orbitAngle: 45,
-    elevation: 20,
-    distance: 700
+    orbitRadius: 60
   },
 
   {
-    id: 1,
-    title: "Right Side Perspective",
     image: "images/img2.jpg",
     orbitAngle: 90,
-    elevation: 18,
-    distance: 650
+    orbitRadius: 90
   },
 
   {
-    id: 2,
-    title: "Rear Left Perspective",
     image: "images/img3.jpg",
     orbitAngle: 220,
-    elevation: 22,
-    distance: 800
+    orbitRadius: 70
   }
 
 ];
