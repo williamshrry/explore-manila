0c7f3a3e635132bb651741226f1107935288484e
williamshrry <williamsherbert326@gmail.com>
2026-05-11 22:48:28 +0800

    Update photos.js

--- photos.js
+++ photos.js
@@ -1,59 +1,30 @@
-/* =========================
-FILE: photos.js
-========================= */
-
 const photos = [
 
   {
     id: 0,
     title: "Front Right Perspective",
     image: "images/img1.jpg",
-
-    orbitAngle: 175,
+    orbitAngle: 45,
     elevation: 20,
-    distance: 700,
-
-    description:
-      "Elevated front-right aerial perspective overlooking the Pasig River and Manila Bay."
+    distance: 700
   },
 
   {
     id: 1,
     title: "Right Side Perspective",
     image: "images/img2.jpg",
-
-    orbitAngle: 100,
+    orbitAngle: 90,
     elevation: 18,
-    distance: 650,
-
-    description:
-      "Direct right-side perspective of the Manila Post Office."
+    distance: 650
   },
 
   {
     id: 2,
     title: "Rear Left Perspective",
     image: "images/img3.jpg",
-
-    orbitAngle: 50,
+    orbitAngle: 220,
     elevation: 22,
-    distance: 800,
-
-    description:
-      "Rear-left aerial perspective with deeper urban backdrop."
-  },
-
-  {
-    id: 3,
-    title: "Rear Left Perspective",
-    image: "images/img4.jpg",
-
-    orbitAngle: 310,
-    elevation: 22,
-    distance: 800,
-
-    description:
-      "Rear-left aerial perspective with deeper urban backdrop."
+    distance: 800
   }
 
 ];
