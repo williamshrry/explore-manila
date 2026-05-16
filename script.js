c5d71c26750a56541bbf793e4e0a1b376de3a415
williamshrry <williamsherbert326@gmail.com>
2026-05-16 09:45:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -54,90 +54,13 @@ const photoNodes = [
   },
 
     {
-    id: 5,
-    title: "View 5",
-
-    image: "images/img5.jpg",
-
-    angle: 185,
-    distance: 170,
-    elevation: 120
-  },
-
-  {
-    id: 6,
-    title: "View 6",
-
-    image: "images/img6.jpg",
-
-    angle: 295,
-    distance: 80,
-    elevation: 10
-  },
-
-   {
-    id: 7,
-    title: "View 7",
-
-    image: "images/img7.jpg",
-
-    angle: 250,
-    distance: 30,
-    elevation: 10
-  },
-
-   {
-    id: 8,
-    title: "View 8",
-
-    image: "images/img8.jpg",
-
-    angle: 140,
-    distance: 12,
-    elevation: 10
-  },
-
-    {
     id: 9,
     title: "View 9",
 
     image: "images/img9.jpg",
 
-    angle: 60,
-    distance: 210,
-    elevation: 70
-  },
-
-    {
-    id: 10,
-    title: "View 10",
-
-    image: "images/img10.jpg",
-
-    angle: 210,
-    distance: 120,
-    elevation: 160
-  },
-
- {
-    id: 11,
-    title: "View 11",
-
-    image: "images/img11.jpg",
-
-    angle: 270,
-    distance: 330,
-    elevation: 120
-  },
-
-   {
-    id: 12,
-    title: "View 12",
-
-    image: "images/img12.jpg",
-
-    angle: 230,
-    distance: 280,
+    angle: 185,
+    distance: 170,
     elevation: 120
   },
 
@@ -147,16 +70,93 @@ const photoNodes = [
 
     image: "images/img13.jpg",
 
+    angle: 295,
+    distance: 80,
+    elevation: 10
+  },
+
+   {
+    id: 12,
+    title: "View 12",
+
+    image: "images/img12.jpg",
+
+    angle: 250,
+    distance: 30,
+    elevation: 10
+  },
+
+   {
+    id: 10,
+    title: "View 10",
+
+    image: "images/img10.jpg",
+
+    angle: 140,
+    distance: 12,
+    elevation: 10
+  },
+
+    {
+    id: 28,
+    title: "View 28",
+
+    image: "images/img28.jpg",
+
+    angle: 60,
+    distance: 210,
+    elevation: 70
+  },
+
+    {
+    id: 8,
+    title: "View 8",
+
+    image: "images/img8.jpg",
+
+    angle: 210,
+    distance: 120,
+    elevation: 160
+  },
+
+ {
+    id: 6,
+    title: "View 6",
+
+    image: "images/img6.jpg",
+
+    angle: 270,
+    distance: 330,
+    elevation: 120
+  },
+
+   {
+    id: 7,
+    title: "View 7",
+
+    image: "images/img7.jpg",
+
+    angle: 230,
+    distance: 280,
+    elevation: 120
+  },
+
+  {
+    id: 11,
+    title: "View 11",
+
+    image: "images/img11.jpg",
+
     angle: 215,
     distance: 68,
     elevation: 10
   },
 
      {
-    id: 14,
-    title: "View 14",
+    id: 29,
+    title: "View 29",
 
-    image: "images/img14.jpg",
+    image: "images/img29.jpg",
 
     angle: 70,
     distance: 140,
@@ -164,10 +164,10 @@ const photoNodes = [
   },
 
    {
-    id: 15,
-    title: "View 15",
+    id: 16,
+    title: "View 16",
 
-    image: "images/img15.jpg",
+    image: "images/img16.jpg",
 
     angle: 310,
     distance: 97,
@@ -175,10 +175,10 @@ const photoNodes = [
   },
 
   {
-    id: 16,
-    title: "View 16",
+    id: 5,
+    title: "View 5",
 
-    image: "images/img16.jpg",
+    image: "images/img5.jpg",
 
     angle: 285,
     distance: 330,
@@ -186,10 +186,10 @@ const photoNodes = [
   },
 
    {
-    id: 17,
-    title: "View 17",
+    id: 15,
+    title: "View 15",
 
-    image: "images/img17.jpg",
+    image: "images/img15.jpg",
 
     angle: 300,
     distance: 180,
@@ -208,10 +208,10 @@ const photoNodes = [
   },
 
     {
-    id: 19,
-    title: "View 19",
+    id: 17,
+    title: "View 17",
 
-    image: "images/img19.jpg",
+    image: "images/img17.jpg",
 
     angle: 318,
     distance: 124,
@@ -230,24 +230,13 @@ const photoNodes = [
   },
 
    {
-    id: 21,
-    title: "View 21",
+    id: 25,
+    title: "View 25",
 
-    image: "images/img21.jpg",
+    image: "images/img25.jpg",
 
     angle: 50,
     distance: 243,
-    elevation: 10
-  },
-
-   {
-    id: 22,
-    title: "View 22",
-
-    image: "images/img22.jpg",
-
-    angle: 38,
-    distance: 240,
     elevation: 10
   },
 
@@ -256,6 +245,17 @@ const photoNodes = [
     title: "View 23",
 
     image: "images/img23.jpg",
+
+    angle: 38,
+    distance: 240,
+    elevation: 10
+  },
+
+   {
+    id: 21,
+    title: "View 21",
+
+    image: "images/img21.jpg",
 
     angle: 365,
     distance: 148,
@@ -274,13 +274,24 @@ const photoNodes = [
   },
 
   {
-    id: 25,
-    title: "View 25",
+    id: 19,
+    title: "View 19",
 
-    image: "images/img25.jpg",
+    image: "images/img19.jpg",
 
     angle: 325,
     distance: 150,
+    elevation: 10
+  },
+
+   {
+    id: 22,
+    title: "View 22",
+
+    image: "images/img22.jpg",
+
+    angle: 15,
+    distance: 187,
     elevation: 10
   },
 
@@ -290,8 +301,19 @@ const photoNodes = [
 
     image: "images/img26.jpg",
 
-    angle: 15,
-    distance: 187,
+    angle: 65,
+    distance: 297,
+    elevation: 10
+  },
+
+  {
+    id: 14,
+    title: "View 14",
+
+    image: "images/img14.jpg",
+
+    angle: 278,
+    distance: 210,
     elevation: 10
   },
 
@@ -300,28 +322,6 @@ const photoNodes = [
     title: "View 27",
 
     image: "images/img27.jpg",
-
-    angle: 65,
-    distance: 297,
-    elevation: 10
-  },
-
-  {
-    id: 28,
-    title: "View 28",
-
-    image: "images/img28.jpg",
-
-    angle: 278,
-    distance: 210,
-    elevation: 10
-  },
-
-   {
-    id: 29,
-    title: "View 29",
-
-    image: "images/img29.jpg",
 
     angle: 70,
     distance: 300,
